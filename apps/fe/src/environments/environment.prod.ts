e9dff8093adc59d680e9e5d8a5e1af2fe41d7a72
Miroshnichenko Vladislav <vm@sourceangel.dk>
2026-03-02 22:22:19 +0200

    added files in ignore

--- apps/fe/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  staging: false,
-  apiUrl: 'https://yourdomain.com',
-};
--- apps/fe/src/environments/environment.staging.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  staging: true,
-  apiUrl: 'https://staging.yourdomain.com',
-};
